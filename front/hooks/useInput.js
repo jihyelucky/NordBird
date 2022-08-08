0807fbf4f614dda8821f6a9dc4423b0c0e7c6232
jihyelucky <94450652+jihyelucky@users.noreply.github.com>
2022-08-08 16:26:26 +0900

    Delete front directory

--- front/hooks/useInput.js
+++
@@ -1,9 +0,0 @@
-import { useCallback, useState } from "react";
-
-export default (initValue = null) => {
-  const [value, setValue] = useState(initValue);
-  const handler = useCallback((e) => {
-    setValue(e.target.value);
-  }, []);
-  return [value, handler, setValue];
-};
